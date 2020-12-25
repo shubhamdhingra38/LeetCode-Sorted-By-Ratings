309b3464576f39b28e2d79d3f410cc6a06168da9
Shubham <dhingra.shubham38@gmail.com>
2020-12-26 04:55:19 +0530

    Added frontend and async calls to API Fetches all data now in <5s :) Set scheduler to update every few hours

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from flask import Flask
 import requests
 import atexit
 import time
+from flask import render_template
 #https://stackoverflow.com/questions/21214270/how-to-schedule-a-function-to-run-every-hour-on-flask
 from apscheduler.schedulers.background import BackgroundScheduler 
 import pickle
+import json
+from math import sqrt
+import asyncio
+from aiohttp import ClientSession
 
 
 with open('./query_string.txt') as f:
     query_string = f.read()
+
+
 
 app = Flask(__name__)
 
 
 API_URLS = {
     'questions_list': "https://leetcode.com/api/problems/algorithms/",
-    'question_info': 'https://leetcode.com/graphql',
+    'question_info': 'http://leetcode.com/graphql',
 }
 
 def print_date_time():
@@ -31,12 +38,11 @@ def make_query(query, variables, url):
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(
             request.status_code, query))
-from math import sqrt
 
 
 
 class Question:
-    def __init__(self, q_id, name, difficulty, submissions, likes, dislikes):
+    def __init__(self, q_id, name, difficulty, submissions, likes, dislikes, slug, stats):
         self.q_id = q_id
         self.name = name
         self.difficulty = difficulty
@@ -44,6 +50,8 @@ class Question:
         self.likes = likes
         self.dislikes = dislikes
         self.ratio = Question.calculate_like_dislike_ratio(likes, dislikes)
+        self.slug = slug
+        self.stats = stats
 
     @staticmethod
     def calculate_like_dislike_ratio(likes, dislikes):
@@ -64,27 +72,47 @@ class Question:
          return self.get_rating() < other.get_rating()
 
     def __str__(self):
-        return f'Question {self.q_id}: {self.name} with a ratio of likes/dislikes {self.ratio}'
+        return f'Question {self.q_id}: {self.name} with a ratio of likes/dislikes {self.ratio:.2f}'
 
+
+
+with open('./saved_results.pkl', 'rb') as f:
+        questions = pickle.load(f)
 
 @app.route('/')
 def home():
     return 'LeetCode most liked questions'
 
-
 @app.route('/most-liked/<int:top_k>')
 def get_most_liked(top_k):
-    with open('./saved_results.pkl', 'rb') as f:
-        questions = pickle.load(f)
-    results = []
-    for q_idx in range(min(top_k, len(questions))):
-        results.append(str(questions[q_idx]))
-    return '\n'.join(results)
+    return render_template('index.html', questions=questions)
 
-def save_most_liked(top_k=10):
+
+async def fetch(url, session, variables, query):
+    async with session.post(url, json={'query': query, 'variables': variables}) as response:
+        return await response.read()
+
+async def run(query_string, list_variables, url):
+    tasks = []
+
+    # Fetch all responses within one Client session,
+    # keep connection alive for all requests.
+    async with ClientSession() as session:
+        for i in range(len(list_variables)):
+            variables = list_variables[i]
+            task = asyncio.ensure_future(fetch(url, session, variables=variables, query=query_string))
+            tasks.append(task)
+
+        responses = await asyncio.gather(*tasks)
+        # you now have all response bodies in this variable
+        # print(responses[-1])
+        return responses
+
+def save_most_liked(top_k=-1):
     print('=' * 50)
     print("Starting a new fetch cycle for updating stored results...")
     print_date_time()
+
     response = requests.get(API_URLS['questions_list'])
 
     if not response.ok:
@@ -93,46 +121,61 @@ def save_most_liked(top_k=10):
     json_response = response.json()
     list_of_questions = json_response['stat_status_pairs']
     questions = []
+    if top_k == -1:
+        top_k = len(list_of_questions)
+    
+    list_variables = []
 
     for question in list_of_questions[:top_k]:
-        difficulty_info = question['difficulty']
         stat_info = question['stat']
         if question['paid_only']:
             print(f"Skipping paid only question {stat_info['question_id']}...")
             continue
+        list_variables.append({"titleSlug": stat_info['question__title_slug']})
+    
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run(query_string, list_variables, API_URLS['question_info']))
+    responses = map(json.loads, loop.run_until_complete(future))
 
-        try:
-            result = make_query(query_string, variables={
-                                "titleSlug": stat_info['question__title_slug']}, url=API_URLS['question_info'])
-        except:
-            print('Some error occured')
-            continue
-        result = result['data']['question']
+
+    for response, question in zip(responses, list_of_questions):
+        difficulty_info = question['difficulty']
+        stat_info = question['stat']
+        result = response['data']['question']
         q = Question(q_id=stat_info['question_id'], difficulty=difficulty_info['level'], name=stat_info['question__title'],
-                     submissions=stat_info['total_submitted'], likes=result['likes'], dislikes=result['dislikes'])
+                     submissions=stat_info['total_submitted'], likes=result['likes'], dislikes=result['dislikes'], slug=result['titleSlug'],
+                     stats=json.loads(result['stats']))
         questions.append(q)
+
 
     print(f"Got {len(questions)} questions...")
     print("Sorting...")
     questions.sort(reverse=True)
     print("Printing fetched questions...")
+
     for q in questions:
         print(q)
+        if q == None:
+            print("BAD")
+
     print("Saving to file...")
-    with open('saved_results.pkl', 'wb') as f:
+    with open('./saved_results.pkl', 'wb') as f:
         pickle.dump(questions, f)
     print('=' * 50)
 
 
 
+EVERY_N_HOUR = 5
+
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=save_most_liked, trigger="interval", seconds=300)
+scheduler.add_job(func=save_most_liked, trigger="interval", minutes=EVERY_N_HOUR * 60)
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown()) #close on shutdown
 
+# save_most_liked(-1) #call this for fetching all questions
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=1337)
